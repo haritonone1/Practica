6c2978ba4030a806a2dc7842a9a53751e9ef747a
Hariton <haritonone1@gmail.com>
2020-04-20 23:53:35 +0300

    First commit

---
+++ JS/Index.js
@@ -0,0 +1,110 @@
+﻿
+var getJson = function()
+{
+    var xhttp;
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            useJson(this);
+        }
+    };
+
+    xhttp.open("GET", `../package.json`, true);
+    xhttp.send();
+}
+
+getJson();
+
+var homepageVacances;
+var products;
+function useJson(xhttp) {
+    products = JSON.parse(xhttp.responseText);
+    addElementsToShop(products,0,3,3)
+    addExtremeVacances(products,0,2);
+}
+
+function addElementsToShop(jsonDataArray,arrayStart,itemsToSpawn,maxElementsInRow)
+{
+    var hotOffersMenus = document.getElementsByClassName("hotOffersMenu");
+    var vacancesPassed = 0;
+    var cycleLimit;
+    
+    if((itemsToSpawn + arrayStart) > jsonDataArray.length){
+        cycleLimit = jsonDataArray.length;
+    }
+    else cycleLimit = itemsToSpawn + arrayStart;
+    
+    for(var j=0;j<hotOffersMenus.length;j++)
+    {   
+        var hotOffersMenu = hotOffersMenus[j];
+        for (var i = vacancesPassed; i < maxElementsInRow+vacancesPassed; i++) {
+            $(hotOffersMenu).append('<div class="menuItem"></div>');
+            homepageVacances = $(".menuItem");
+            $(homepageVacances[i]).append(`<p class="hotOffersMenuItemName">${jsonDataArray[i].Name}</p>`);
+            $(homepageVacances[i]).append('<div class="borderBottom"></div>');
+            $(homepageVacances[i]).append('<br>');
+            $(homepageVacances[i]).append('<img class="homepageVacanceImage">');
+            $(homepageVacances[i]).children(".homepageVacanceImage").attr("src", jsonDataArray[i].PhotoURL);
+        }
+        vacancesPassed += maxElementsInRow;
+    }
+}
+
+function addExtremeVacances(jsonDataArray,arrayStart,maxElementsInRow)
+{
+    var vacancesPassed = 0;
+    var itemsToSpawn = 0;
+    var extremeItems = [];
+
+    for(var j=0;j<jsonDataArray.length;j++){
+        if(jsonDataArray[j].Extreme == true){
+            extremeItems[itemsToSpawn] = jsonDataArray[j];
+            itemsToSpawn++;
+        }
+    }
+    console.log(extremeItems);
+    
+    if(itemsToSpawn >= 2){
+        $(".extremeTop").append('<div class="image40"></div>');
+        imageDiv = $(".image40");
+        $(imageDiv).append('<img class="extremeImage">');
+        $(imageDiv).children(".extremeImage").attr("src",extremeItems[0].PhotoURL);
+
+        $(".extremeTop").append('<div class="image50"></div>');
+        imageDiv = $(".image50");
+        $(imageDiv).append('<img class="extremeImage">');
+        $(imageDiv).children(".extremeImage").attr("src",extremeItems[1].PhotoURL);
+    }
+    
+    else if(itemsToSpawn == 1){
+
+        $(".extremeTop").append('<div class="image80"></div>');
+        imageDiv = $(".image80");
+        $(imageDiv).append('<img class="extremeImage">');
+        $(imageDiv).children(".extremeImage").attr("src",extremeItems[0].PhotoURL);
+    }
+    
+    
+    
+    
+
+
+    // var hotOffersMenu = hotOffersMenus[j];
+    // for (var i = vacancesPassed; i < maxElementsInRow+vacancesPassed; i++) {
+    //     $(hotOffersMenu).append('<div class="menuItem"></div>');
+    //     homepageVacances = $(".menuItem");
+    //     $(homepageVacances[i]).append(`<p class="hotOffersMenuItemName">${jsonDataArray[i].Name}</p>`);
+    //     $(homepageVacances[i]).append('<div class="borderBottom"></div>');
+    //     $(homepageVacances[i]).append('<br>');
+    //     $(homepageVacances[i]).append('<img class="homepageVacanceImage">');
+    //     $(homepageVacances[i]).children(".homepageVacanceImage").attr("src", jsonDataArray[i].PhotoURL);
+    // }
+}
+
+function addShopElements()
+{
+    addElementsToShop(products,shopElements.length,3); //onclick function
+}
+
+
+
---
+++ JS/Shop.js
@@ -0,0 +1,67 @@
+﻿
+var getJson = function()
+{
+
+    var xhttp;
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            useJson(this);
+        }
+    };
+
+    xhttp.open("GET", `../package.json`, true);
+    xhttp.send();
+}
+
+getJson();
+var shopElements;
+var products;
+function useJson(xhttp) {
+    products = JSON.parse(xhttp.responseText);
+    addElementsToShop(products,0,2)
+}
+
+function addElementsToShop(jsonDataArray,arrayStart,itemsToSpawn)
+{
+    var cycleLimit;
+    var turnOffButton = false;
+    if((itemsToSpawn + arrayStart) >= jsonDataArray.length){
+        cycleLimit = jsonDataArray.length;
+        turnOffButton = true;
+    }
+    else cycleLimit = itemsToSpawn + arrayStart;
+    
+    for(var i=arrayStart;i<cycleLimit;i++)
+    {
+        $(".mainShop").append('<div class="shopItem"></div>');
+        shopElements = $(".shopItem");
+        $(shopElements[i]).append('<div class="vacanceImageDiv"></div>');
+        $(shopElements[i]).children(".vacanceImageDiv").append('<img class="vacanceImage">');
+        $(shopElements[i]).children(".vacanceImageDiv").children(".vacanceImage").attr("src", jsonDataArray[i].PhotoURL);
+        $(shopElements[i]).append('<div class="vacanceData"></div>');
+        $(shopElements[i]).children(".vacanceData").append(`<p class="vacanceName">${jsonDataArray[i].Name}</p>`);
+        $(shopElements[i]).children(".vacanceData").append('<div class="vacanceDetails"></div>');
+        $(shopElements[i]).children(".vacanceData").children(".vacanceDetails").append('<img class="vacanceDetailIcon" src="Images/human.png">');
+        $(shopElements[i]).children(".vacanceData").children(".vacanceDetails").append(`<p class="vacanceDetailData">${jsonDataArray[i].Persons}</p>`);
+        $(shopElements[i]).children(".vacanceData").children(".vacanceDetails").append('<img class="vacanceDetailIcon" src="Images/moon.png">');
+        $(shopElements[i]).children(".vacanceData").children(".vacanceDetails").append(`<p class="vacanceDetailData">${jsonDataArray[i].Nights}</p>`);
+        $(shopElements[i]).children(".vacanceData").append(`<p class="vacancePrice">${jsonDataArray[i].Price + "$ per person"}</p>`);
+        $(shopElements[i]).children(".vacanceData").append(`<button class="orderVacance"></button>`);
+        $(shopElements[i]).children(".vacanceData").children(".orderVacance").append(`<p class="startTripText">Start Your Trip</p>`);
+    }
+    if(turnOffButton)
+    {
+        $("#showMoreButton").css(
+            "display" , "none"
+        )
+    }
+}
+
+function addShopElements()
+{
+    addElementsToShop(products,shopElements.length,2); //onclick function
+}
+
+
+
