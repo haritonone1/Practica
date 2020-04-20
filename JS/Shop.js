f68d3572f2d67e62fc9700d12a3ab885cd748db4
Hariton <haritonone1@gmail.com>
2020-04-21 01:46:30 +0300

    Sort added

--- JS/Shop.js
+++ JS/Shop.js
@@ -16,9 +16,12 @@ var getJson = function()
 
 getJson();
 var shopElements;
+var sortedJSONData = [];
 var products;
 function useJson(xhttp) {
     products = JSON.parse(xhttp.responseText);
+    sortedJSONData = products;
+    unsortedJSONData = sortedJSONData;
     addElementsToShop(products,0,2)
 }
 
@@ -43,9 +46,9 @@ function addElementsToShop(jsonDataArray,arrayStart,itemsToSpawn)
         $(shopElements[i]).children(".vacanceData").append(`<p class="vacanceName">${jsonDataArray[i].Name}</p>`);
         $(shopElements[i]).children(".vacanceData").append('<div class="vacanceDetails"></div>');
         $(shopElements[i]).children(".vacanceData").children(".vacanceDetails").append('<img class="vacanceDetailIcon" src="Images/human.png">');
-        $(shopElements[i]).children(".vacanceData").children(".vacanceDetails").append(`<p class="vacanceDetailData">${jsonDataArray[i].Persons}</p>`);
+        $(shopElements[i]).children(".vacanceData").children(".vacanceDetails").append(`<p class="vacanceDetailData1">${jsonDataArray[i].Persons}</p>`);
         $(shopElements[i]).children(".vacanceData").children(".vacanceDetails").append('<img class="vacanceDetailIcon" src="Images/moon.png">');
-        $(shopElements[i]).children(".vacanceData").children(".vacanceDetails").append(`<p class="vacanceDetailData">${jsonDataArray[i].Nights}</p>`);
+        $(shopElements[i]).children(".vacanceData").children(".vacanceDetails").append(`<p class="vacanceDetailData2">${jsonDataArray[i].Nights}</p>`);
         $(shopElements[i]).children(".vacanceData").append(`<p class="vacancePrice">${jsonDataArray[i].Price + "$ per person"}</p>`);
         $(shopElements[i]).children(".vacanceData").append(`<button class="orderVacance"></button>`);
         $(shopElements[i]).children(".vacanceData").children(".orderVacance").append(`<p class="startTripText">Start Your Trip</p>`);
@@ -56,6 +59,59 @@ function addElementsToShop(jsonDataArray,arrayStart,itemsToSpawn)
             "display" , "none"
         )
     }
+}
+var sortState = 0;
+
+function changeObjectData(){
+    if(sortState == 0){
+        multiplier = 1;
+    }
+    else if(sortState == 1){
+        multiplier = -1;
+    }
+    else {}
+    sortData(multiplier);
+    let shopItems = document.getElementsByClassName("shopItem");
+    for(let i=0;i<shopItems.length;i++){
+        $(shopItems[i]).children(".vacanceImageDiv").children(".vacanceImage").attr("src",sortedJSONData[i].PhotoURL);
+        $(shopItems[i]).children(".vacanceData").children(".vacanceName").text(sortedJSONData[i].Name);
+        $(shopItems[i]).children(".vacanceData").children(".vacanceDetails").children(".vacanceDetailData1").text(sortedJSONData[i].Persons);
+        $(shopItems[i]).children(".vacanceData").children(".vacanceDetails").children(".vacanceDetailData2").text(sortedJSONData[i].Nights);
+        $(shopElements[i]).children(".vacanceData").children(".vacancePrice").text(sortedJSONData[i].Price + "$ per person");
+    }
+    sortState++;
+    // if(sortState > 2){
+    //     for(let i=0;i<shopItems.length;i++) {
+    //         $(shopItems[i]).children(".vacanceImageDiv").children(".vacanceImage").attr("src", unsortedJSONData[i].PhotoURL);
+    //         $(shopItems[i]).children(".vacanceData").children(".vacanceName").text(unsortedJSONData[i].Name);
+    //         $(shopItems[i]).children(".vacanceData").children(".vacanceDetails").children(".vacanceDetailData1").text(unsortedJSONData[i].Persons);
+    //         $(shopItems[i]).children(".vacanceData").children(".vacanceDetails").children(".vacanceDetailData2").text(unsortedJSONData[i].Nights);
+    //         $(shopElements[i]).children(".vacanceData").children(".vacancePrice").text(unsortedJSONData[i].Price + "$ per person");
+    //     }
+    //     sortState=0;
+    // }
+    if(sortState>1){
+        sortState =0;
+    }
+
+}
+
+
+
+function sortData(multiplier){
+    sortedJSONData.sort(function (a,b) {
+        if(a.Price > b.Price)
+            return 1 * multiplier;
+        
+        if(a.Price < b.Price){
+            return -1 * multiplier;
+        }
+        
+        if(a.Price == b.Price){
+            return 0;
+        }
+    })
+    
 }
 
 function addShopElements()
